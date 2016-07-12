e23ce98d72384275ad568893cc0c42cae128d46e
Leo Lorenzo Luis <leolorenzoluis@outlook.com>
2016-07-12 13:49:08 -0400

    Restructured and added index file

---
+++ server/Dataset.fs
@@ -0,0 +1,22 @@
+﻿namespace Dataset
+    open FSharp.Data
+
+    module Elementary =
+        type Elementary = {
+            Region : string
+            Province : string
+            Municipality : string
+            Division : string
+            SchoolId : int
+            }
+
+        type private Provider = CsvProvider<"data/elementary.csv">
+        let private providerContext = Provider.Load("data/elementary.csv")
+        let value = providerContext.Rows
+                            |> Seq.map (fun row -> {
+                                                        Region = row.Region
+                                                        Province = row.Province
+                                                        Municipality = row.Municipality
+                                                        Division = row.Division
+                                                        SchoolId = row.School_id
+                                                    })
